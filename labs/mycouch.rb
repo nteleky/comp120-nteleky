6bf5eec48387c2fab5df11bc077b0166fcfc4c3c
Nick Teleky <nteleky@gmail.com>
2013-09-21 16:04:13 -0400

    Started Lab 1

---
+++ labs/mycouch.rb
@@ -0,0 +1,10 @@
+# Web Engineering: Comp 120
+# Lab 1: Ruby and CouchDB
+# Nicholas Teleky
+# September 21, 2013
+
+# Database server: 67.23.79.113:5984
+
+# Create a database that is the same name as my github handle
+
+# Insert a document into the database
