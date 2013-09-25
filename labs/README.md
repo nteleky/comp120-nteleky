f6fd6b5030549d2b3a8da49ba315fe2474a57828
Nick Teleky <nteleky@gmail.com>
2013-09-24 23:04:56 -0400

    Finished Lab 1

---
+++ labs/README.md
@@ -0,0 +1,10 @@
+Comp120: Labs
+=============
+
+### Lab 1 ###
+
+Files:
+ * mycouch.rb
+
+Purpose:
+ * To use ruby to connect and add documents to a CouchDB.
--- labs/mycouch.rb
+++ labs/mycouch.rb
@@ -1,17 +1,26 @@
 # Web Engineering: Comp 120
 # Lab 1: Ruby and CouchDB
 # Nicholas Teleky
-# September 21, 2013
+# September 24, 2013
 require "CouchDB"
 
-# Create a database that is the same name as my github handle
-server = CouchDB::Server.new "67.23.79.113", 5984
-database = CouchDB::Database.new server, "nteleky"
-database.delete_if_exists!
-database.create_if_missing!
+begin
+	# Create a database that is the same name as my github handle
+	server = CouchDB::Server.new "67.23.79.113", 5984
+	database = CouchDB::Database.new server, "nteleky"
+	database.delete_if_exists!
+	database.create_if_missing!
+rescue
+	print "Database couldn't be created"
 
-# Insert a document into the database
-document_one = CouchDB::Document.new database, "_id" => "Doc 1", "type" => "testdoc"
-document_one.save
+end
 
-print server.database_names()
+begin
+	# Insert a document into the database
+	document_one = CouchDB::Document.new database, "_id" => "Doc 1", "type" => "testdoc"
+	document_two = CouchDB::Document.new database, "_id" => "Doc 2", "type" => "testdoc"
+	document_one.save
+	document_two.save
+rescue
+	print "documents couldn't be saved"
+end
