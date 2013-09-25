3f4ec139518287f6a469bd28ae83d0115b16552f
Nick Teleky <nteleky@gmail.com>
2013-09-24 22:50:26 -0400

    connects to database and adds a document.

--- labs/mycouch.rb
+++ labs/mycouch.rb
@@ -2,9 +2,16 @@
 # Lab 1: Ruby and CouchDB
 # Nicholas Teleky
 # September 21, 2013
-
-# Database server: 67.23.79.113:5984
+require "CouchDB"
 
 # Create a database that is the same name as my github handle
+server = CouchDB::Server.new "67.23.79.113", 5984
+database = CouchDB::Database.new server, "nteleky"
+database.delete_if_exists!
+database.create_if_missing!
 
 # Insert a document into the database
+document_one = CouchDB::Document.new database, "_id" => "Doc 1", "type" => "testdoc"
+document_one.save
+
+print server.database_names()
