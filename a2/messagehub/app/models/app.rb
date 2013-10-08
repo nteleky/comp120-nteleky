3658493cbd5dea465785c386b2dc0b78f2d13cfe
Nick Teleky <nteleky@gmail.com>
2013-10-08 17:29:34 -0400

    Create new controller called messages

--- a2/messagehub/app/models/app.rb
+++ a2/messagehub/app/models/app.rb
@@ -1,2 +1,4 @@
 class App < ActiveRecord::Base
+	attr_accessible :app_title, :description
+	has_many :messages
 end
--- a2/messagehub/config/routes.rb
+++ a2/messagehub/config/routes.rb
@@ -1,4 +1,7 @@
 Messagehub::Application.routes.draw do
+  get "messages/index"
+  get "messages/create"
+  get "messages/show"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
