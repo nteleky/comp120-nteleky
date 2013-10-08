fba0096edb28346b18bc93f026506f1f437675fe
Nick Teleky <nteleky@gmail.com>
2013-10-08 01:48:25 -0400

    Create new rails app called messagehub

---
+++ a2/messagehub/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Messagehub::Application.config.secret_key_base = 'e75c71a5d8555fdbd738557fb2994b973b9dbd73d2a23c049248329a7afdccc6d3e21355cf8a80f39a3d7ebe96a6ca87ee2bf9d35247a802f0d2f5d3d79f726d'
